d6cfe1fde8f3b462936904142a35650796c75cc5
Szabó Bence <mentawary@gmail.com>
2022-09-30 15:23:53 +0200

    Utolsó

--- src/hu/petrik/BejegyzesProjekt/Main.java
+++ src/hu/petrik/BejegyzesProjekt/Main.java
@@ -2,9 +2,11 @@ package hu.petrik.BejegyzesProjekt;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     static List<Bejegyzes> bejegyzes = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
 
 
     private static void bejegyzesHozzad(){
